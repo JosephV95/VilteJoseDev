0e3974ac2482107bf6c3c7803b0822313aec9bed
JosephV95 <viltejl@gmail.com>
2023-04-21 21:25:25 -0300

    Se le dio interactividad al botón de menú del nav para el mobile

--- scripts/main.js
+++ scripts/main.js
@@ -90,27 +90,46 @@ const select = (el, all = false) => {
     })
   }
 
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
 
   /**
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('body').classList.toggle('mobile-nav-active')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
+    this.classList.toggle('bx-list-ul')
+    this.classList.toggle('bx-x')
   })
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bx-list-ul')
+        navbarToggle.classList.toggle('bx-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+//   window.addEventListener('load', () => {
+//     if (window.location.hash) {
+//       if (select(window.location.hash)) {
+//         scrollto(window.location.hash)
+//       }
+//     }
+//   });
+
+  
+
+  
