d92a28755ae0f13d3331c5374362516c87b96d30
JosephV95 <viltejl@gmail.com>
2023-11-27 12:35:18 -0300

    Se esta creando el array con la data

---
+++ scripts/miDataJLV.js
@@ -0,0 +1,31 @@
+const dataJLV = {
+  proyectos: [
+    {
+      nombre: "",
+      descripcion: "",
+      img: "",
+      tecnologias: [],
+      demo: "",
+      repo: "",
+    },
+    {
+        nombre: "",
+        descripcion: "",
+        img: "",
+        tecnologias: [],
+        demo: "",
+        repo: "",
+      },
+      {
+        nombre: "Amazing Events",
+        descripcion: `Proyecto Frontend implementado con Vue js (<i>en el repositorio
+            también se encuentra el desarrollo en Javascript Vanilla</i>), se utilizó una API para obtener la
+            información de los eventos. La página dispone de dos tipos de filtrado, que pueden ser usados de manera
+            única como combinada.`,
+        img: "assets/img/proyect3.jpg",
+        tecnologias: ["Vue.js", "Javascript", "HTML5 & CSS3", "Bootstrap"],
+        demo: "https://josephv95.github.io/Mindhub-Javascript-Task/",
+        repo: "https://github.com/JosephV95/Mindhub-Javascript-Task.git",
+      },
+  ]
+}
